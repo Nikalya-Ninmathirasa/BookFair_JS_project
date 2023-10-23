1a40c21e294148a8efa115e8070095f313c56d59
Nikalya_1398 <v.ninmathirasa@gmail.com>
2023-10-23 18:58:42 +0530

    first commit

---
+++ bookfair/script.js
@@ -0,0 +1,46 @@
+//select popupbutton, popupbox, popupOverlay
+
+var popupoverlay = document.querySelector(".popup-overlay")
+var popupbox=document.querySelector(".popup-box")
+var addpopupbutton = document.getElementById("add-popup-button")
+
+
+addpopupbutton.addEventListener("click", function(){
+popupoverlay.style.display="block"
+popupbox.style.display="block"
+
+})
+  
+// select cancel button
+let cancelpopup = document.getElementById("cancel-popup")
+cancelpopup.addEventListener("click", function(event){
+    event.preventDefault()
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+})
+
+//select container, add-book , book-title-input, book-author-input, book-description-input
+let container = document.querySelector(".container")
+let addbook = document.getElementById("add-book")
+let booktitleinput = document.getElementById("book-title-input")
+let bookauthorinput = document.getElementById("book-author-input")
+let bookdescriptioninput = document.getElementById("book-description-input")
+
+addbook.addEventListener("click",function(event){
+    event.preventDefault()
+    let div = document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML = `<h2>${booktitleinput.value}</h2>
+    <h5>${bookauthorinput.value} </h5>
+    <p>${bookdescriptioninput.value}</p>
+    <button onclick='removebook(event)'>Delete</button>`
+    container.append(div)
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+
+})
+
+function removebook(event){
+    event.target.parentElement.remove()
+}
+
